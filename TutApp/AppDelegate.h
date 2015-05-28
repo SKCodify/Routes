cfabc73a17473fb8d30699b6f53d164417f65a2c
Shubham <infoedge@Shubham.local>
2015-05-28 11:11:14 +0530

    Initial Commit

---
+++ TutApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TutApp
+//
+//  Created by Infoedge on 5/28/15.
+//  Copyright (c) 2015 Infoedge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TutApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TutApp
+//
+//  Created by Infoedge on 5/28/15.
+//  Copyright (c) 2015 Infoedge. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
